0973f64f7897cc87d8f8f841d63ea3956aaa6759
wcolts2000 <wcolts2000@gmail.com>
2019-08-15 15:15:22 -0700

    boilerplate communicating with localhost server

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './App.css';
+import axios from 'axios';
+
+class App extends Component {
+  state = {
+    data: []
+  };
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/api/v1')
+      .then(({ data }) => {
+        console.log(data);
+        this.setState({ data });
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>{this.state.data.message}</p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
