72ac4d792a1db00725fe552a0c846541cc2c37b9
Jason Belgard <belgard.jason@gmail.com>
2019-08-20 15:18:08 -0500

    Login redone

--- src/login/Login.js
+++ src/login/Login.js
@@ -1,77 +1,62 @@
-import React, { Component } from 'react';
-import { Button, FormGroup, FormControl, FormLabel } from 'react-bootstrap';
-import { Link } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { withFormik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
 
-export default class Login extends Component {
-    constructor(props) {
-        super(props);
-        this.handleClick = this.handleClick.bind(this);
-        this.state = {
-            username: '',
-            password: ''
-        };
-    }
-    validateForm() {
-        return this.state.username.length > 0 && this.state.password.length > 0;
-    }
-    handleChange = event => {
-        this.setState({
-            [event.target.id]: event.target.value
-        });
-    }
-    handleSubmit = event => {
-        event.preventDefault();
-    }
-    handleClick() {
-        this.setState(state => ({
-            isToggleOn: !state.isToggleOn
-        }));
-    }
-    render() {
-        return (
-            <div className = 'login'>
-                <form onSubmit = {this.handleSubmit}>
-                    <FormGroup controlId = 'username'>
-                        <FormLabel>Username</FormLabel>
-                        <FormControl
-                            autoFocus
-                            type = 'username'
-                            value = {this.state.username}
-                            onChange = {this.handleChange}
-                        />
-                    </FormGroup>
-                    <FormGroup controlId = 'password'>
-                        <FormLabel>Password</FormLabel>
-                        <FormControl
-                            value = {this.state.password}
-                            onChange = {this.handleChange}
-                            type = 'password'
-                        />
-                    </FormGroup>
-                    <Link to = '/dashboard'>
-                        <Button
-                            onClick = {this.handleClick}
-                            block
-                            disabled = {!this.validateForm()}
-                            type = 'submit'
-                        >
-                            Login
-                        </Button>
-                    </Link>
-                    <Link to = '/register'>
-                        <Button 
-                            onClick = {this.handleClick}
-                            block
-                            type = 'submit'
-                        >
-                            Register a New User
-                        </Button>
-                    </Link>
-                </form>
-                
+function LoginForm({ values, errors, touched, isSubmitting }) {
+    return (
+        <Form>
+            <div>
+                {touched.username && errors.username && <p>{errors.username}</p>}
+                <Field type = 'username' name = 'username' placeholder = 'Username' />
             </div>
-        )
-    }
+            <div>
+                {touched.password && errors.password && <p>{errors.password}</p>}
+                <Field type = 'password' name = 'password' placeholder = 'Password' />
+            </div>
+            <label>
+                <Field type = 'checkbox' name = 'tos' checked = {values.tos} />
+                Accept TOS
+            </label>
+            <button disabled = {isSubmitting}>Submit</button>
+        </Form>
+    );
 }
 
-export default Login
+const Login = withFormik({
+    mapPropsToValues({ username, password, tos }) {
+        return {
+            username: username || '',
+            password: password || '',
+            tos: tos || false,
+        };
+    },
+    validationSchema: Yup.object().shape({
+        username: Yup.string()
+            .username('Username not valid')
+            .required('Username is required'),
+        password: Yup.string()
+            .min(8, 'Password must be 8 characters or longer')
+            .required('Password is required')
+    }),
+    handleSubmit(values, { resetForm, setErrors, setSubmitting }) {
+        if (values.username === 'alreadytaken@atb.dev') {
+            setErrors({ username: 'That username is already taken' });
+        } else {
+            axios
+                .post("https://labs-rv-life-staging-1.herokuapp.com/users/login", values)
+                .then(res => {
+                    console.log(res); // data was created successfully and logs to console
+                    resetForm();
+                    setSubmitting(false);
+                })
+                .catch(err => {
+                    console.log(err); // there was an error creating the data and logs to console
+                    setSubmitting(false);
+                });
+        }
+    }
+})(LoginForm);
+
+export default Login;
