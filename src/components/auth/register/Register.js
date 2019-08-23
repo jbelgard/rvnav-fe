41cd21fb630225e43b54349d8ad75fed0452c4f6
wcolts2000 <wcolts2000@gmail.com>
2019-08-22 23:19:31 -0700

    pulling from master

--- src/components/auth/register/Register.js
+++ src/components/auth/register/Register.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { withFormik, Form, Field } from 'formik';
-import { connect } from "react-redux";
+import { connect } from 'react-redux';
 
 // import * as Yup from 'yup';
 import axios from 'axios';
@@ -11,26 +11,25 @@ function RegisterForm({ values, errors, touched, isSubmitting }) {
     <Form>
       <div>
         {touched.username && errors.username && <p>{errors.username}</p>}
-        <Field type = 'username' name = 'username' placeholder = 'Username' />
+        <Field type="username" name="username" placeholder="Username" />
       </div>
       <div>
         {touched.password && errors.password && <p>{errors.password}</p>}
-        <Field type = 'password' name = 'password' placeholder = 'Password' />
+        <Field type="password" name="password" placeholder="Password" />
       </div>
       <div>
         {touched.first_name && errors.first_name && <p>{errors.first_name}</p>}
-        <Field type = 'first_name' name = 'first_name' placeholder = 'First Name' />
+        <Field type="first_name" name="first_name" placeholder="First Name" />
       </div>
-      {/* // need to change name to firstname and email to lastname and add either drop down or radial selectors for 3 types of rv  a, b, c */}
       <div>
         {touched.last_name && errors.last_name && <p>{errors.last_name}</p>}
-        <Field type = 'last_name' name = 'last_name' placeholder = 'Last Name' />
+        <Field type="last_name" name="last_name" placeholder="Last Name" />
       </div>
       {/* <label>
         <Field type = 'checkbox' name = 'tos' checked = {values.tos} />
         Accept TOS
       </label> */}
-      <button disabled = {isSubmitting}>Submit</button>
+      <button disabled={isSubmitting}>Submit</button>
     </Form>
   );
 }
@@ -41,7 +40,7 @@ const Register = withFormik({
       username: username || '',
       password: password || '',
       last_name: last_name || '',
-      first_name: first_name || '',
+      first_name: first_name || ''
       // tos: tos || ''
     };
   },
@@ -60,19 +59,22 @@ const Register = withFormik({
   //     .required('Email is required')
   // }),
   handleSubmit(values, { resetForm, setErrors, setSubmitting }) {
-    console.log("VALUES: ", values)
+    console.log('VALUES: ', values);
     if (values.username === 'alreadytaken@atb.dev') {
       setErrors({ username: 'That username is already taken' });
     } else {
       axios
-        .post("https://labs-rv-life-staging-1.herokuapp.com/users/register", values)
+        .post(
+          'https://labs-rv-life-staging-1.herokuapp.com/users/register',
+          values
+        )
         .then(res => {
-          console.log("Success!!"); // data was created successfully and logs to console
+          console.log('Success!!'); // data was created successfully and logs to console
           resetForm();
           setSubmitting(false);
         })
         .catch(err => {
-          console.log(err);  // there was an error creating the data and logs to console
+          console.log(err); // there was an error creating the data and logs to console
           setSubmitting(false);
         });
     }
