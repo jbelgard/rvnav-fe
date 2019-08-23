d7c05a16af34cc026eae8ea4fe0d1f21a3c4ad4a
wcolts2000 <wcolts2000@gmail.com>
2019-08-23 09:14:02 -0700

    commit before pull of map

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -3,10 +3,10 @@ import Nav from '../nav/Nav';
 
 function Map() {
   return (
-    <>
+    <React.Fragment>
       <Nav />
       <div style={{ paddingTop: 50 }}>MAP VIEW</div>;
-    </>
+    </React.Fragment>
   );
 }
 
