0251100d8059b3c34235188c8d396b124d3c6376
Kevin Sims <kevinsims1@outlook.com>
2019-08-27 09:52:42 -0600

    implemented routing a to b ability

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Nav from '../nav/Nav';
 
+
 class MapPage extends React.Component {
   
 
@@ -12,12 +13,27 @@ class MapPage extends React.Component {
     loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyCntOT_h7ofl_l1NqJEnTt8az4eUdxPP0E&callback=initMap")
     window.initMap = this.initMap
   }
+  calculateAndDisplayRoute = (directionsService, directionsDisplay) => {
+         directionsService.route({
+           origin: document.getElementById('start').value,
+           destination: document.getElementById('end').value,
+           travelMode: 'DRIVING'
+         }, function(response, status) {
+           if(status === 'OK') {
+             directionsDisplay.setDirections(response)
+           } else {
+             window.alert('Directions request failed due to' + status)
+           }
+         })
+       }
   initMap = () => {
+    var directionsService = new window.google.maps.DirectionsService;
+    var directionsDisplay = new window.google.maps.DirectionsRenderer
     var map = new window.google.maps.Map(document.getElementById('map'), {
       center: {lat: -34.397, lng: 150.644},
       zoom: 8
     });
-    var infoWindow = new window.google.maps.InfoWindow;
+    directionsDisplay.setMap(map)
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function(position) {
         var pos = {
@@ -26,28 +42,48 @@ class MapPage extends React.Component {
         };
         //marker for users location
         var marker = new window.google.maps.Marker({map:map, position: pos});
-        infoWindow.open(map);
         map.setCenter(pos);
-      }, function() {
-        handleLocationError(true, infoWindow, map.getCenter());
       });
     } else {
       // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, map.getCenter());
+      console.log("Error finding location")
     }
-
-    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-      infoWindow.setPosition(pos);
-      infoWindow.setContent(browserHasGeolocation ?
-                            'Error: The Geolocation service failed.' :
-                            'Error: Your browser doesn\'t support geolocation.');
-      infoWindow.open(map);
+     var onChangeHandler = (e) => {
+      e.preventDefault()
+      this.calculateAndDisplayRoute(directionsService, directionsDisplay)
     }
+    document.getElementById('start').addEventListener('change', onChangeHandler)
+    document.getElementById('end').addEventListener('change', onChangeHandler)
   }
+
   render(){
   return (
     <div>
       <Nav />
+      <div className="floatingPanel" 
+      style={{
+        position: 'absolute',
+        top: '10px',
+        left: '25%',
+        zIndex: '5',
+        backgroundColor: '#fff',
+        paddinf: '1rem',
+        border: '1px solid #999',
+        textAlign: 'center',
+        lineHeight: '2rem',
+        paddingLeft: '10px'
+      }}>
+        <b>Start: </b>
+        <select id="start">
+          <option value="chicago, il">Chicago</option>
+          <option value="los angles, ca">los angeles</option>
+        </select>
+        <b>End: </b>
+        <select id="end">
+          <option value="los angles, ca">LA </option>
+          <option value="chicago, il">Chicago</option>
+        </select>
+      </div>
       <div id="map" style={{height: "100vh"}}></div>
     </div>
   );
