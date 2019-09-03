ce18bd9eb692dcb4de69ab85770fae6a4bc4bdda
Mitchell Robles <mitchellr92@live.com>
2019-09-03 11:11:07 -0700

    Rearranged some styling, added app description and link for an about us page

--- src/components/landingPage/LandingPage.js
+++ src/components/landingPage/LandingPage.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Button from 'react-bootstrap/Button';
-import './LandingPage.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import "./LandingPage.css";
 
 const LandingPage = () => {
   return (
     <div className="landing-page-wrapper">
-      <h1>RV Life</h1>
-      <Link to="/auth">
-        <Button variant="warning">Log in / Sign up</Button>
-      </Link>
-      <Link to="/map">
-        <Button variant="light">Pick Route</Button>
+      <div className="button-container">
+        <Link to="/map">
+          <Button variant="light">Use as guest</Button>
+        </Link>
+        <Link to="/auth">
+          <Button variant="warning">Log in / Sign up</Button>
+        </Link>
+      </div>
+      <div className="intro">
+        <h1>RV Life</h1>
+        <p>
+          Welcome to RV Life. The app that helps you travel safely in you RV. We
+          take into account factors like vehicle size and weight to give you the
+          best route! And show points of interest along the way. Try our app
+          today to get the best navigation for you and your vehicle.
+        </p>
+      </div>
+      <Link to="/" className="about">
+        <a>About the team</a>
       </Link>
     </div>
   );
