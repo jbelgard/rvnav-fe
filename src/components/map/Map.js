7e03b7d3e8c14b0078a7b53c3e59eb4c7e5e7090
Kevin Sims <kevinsims1@outlook.com>
2019-09-05 14:46:47 -0500

    nav bar showing with new map

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -52,73 +52,6 @@ class MapPage extends Component {
     var ui = new window.H.ui.UI.createDefault(this.map, layer)
 }    
 
-  // renderMap = () => {
-  //   loadScript(`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLEMAP}&callback=initMap`)
-  //   window.initMap = this.initMap
-  // }
-
-  // calculateAndDisplayRoute = (directionsS, directionsD) => {
-  //   directionsS.route({
-  //     origin: this.state.start,
-  //     destination: this.state.end,
-  //     travelMode: 'DRIVING'
-  //   }, function(response, status) {
-  //       if(status === 'OK') {
-  //         directionsD.setDirections(response)
-  //       } else {
-  //         window.alert('Directions request failed due to' + status)
-  //       }
-
-  //   })
-  // }
-
-  // initMap = (mart) => {
-  //   var directionsService = new window.google.maps.DirectionsService();
-  //   var directionsDisplay = new window.google.maps.DirectionsRenderer();
-  //   var map = new window.google.maps.Map(document.getElementById('map'), {
-  //     center: {lat: -34.397, lng: 150.644},
-  //     zoom: 8
-  //   });
-
-  //     this.setState({
-  //       directionsService,
-  //       directionsDisplay 
-  //     })
-
-
-  //   directionsDisplay.setMap(map)
-  //   if (navigator.geolocation) {
-  //     navigator.geolocation.getCurrentPosition(function(position) {
-  //       var pos = {
-  //         lat: position.coords.latitude,
-  //         lng: position.coords.longitude
-  //       };
-  //       //marker for users location
-  //       new window.google.maps.Marker({map:map, position: pos});
-  //       //new window.google.maps.Marker({map:map, position: mart});
-  //       map.setCenter(pos);
-  //     });
-  //   } else {
-  //     // Browser doesn't support Geolocation
-  //     console.log("Error finding location")
-  //   }
-  //   this.onChangeHandler();
-    
-  //   document.querySelector('form').addEventListener('submit', this.onChangeHandler)
-  // }
-  // routeChangeHandler = (e) => {
-  //  this.setState({
-  //    [e.target.name]: e.target.value
-  //   })
-  // }
-
-  // onChangeHandler = () => {
-  //   // e.preventDefault()
-  //   if(this.state.start.length > 0){
-  //     this.calculateAndDisplayRoute(this.state.directionsService, this.state.directionsDisplay)
-  //   }
-    
-  // }
   render(){
   return (
     <div>
@@ -136,22 +69,13 @@ class MapPage extends Component {
       start={this.state.start}
       end={this.state.end}
       toggleSidebar = {this.toggleSidebar} sidebarOpen = {this.state.sidebarOpen} />
-      <Location />
-      <div id="here-map" style={{width: '100%', height: '400px', background: 'grey' }} />
+
+      <div id="here-map" location={this.Location} style={{width: '100%', height: '400px', background: 'grey' }} />
       {/* <div id="map" ></div> */}
     </div>
   );
 }
 }
 
-// function loadScript(url){
-//   var index = window.document.getElementsByTagName("script")[0]
-//   var script = window.document.createElement("script")
-//   script.src = url
-//   script.async = true
-//   script.defer = true
-//   index.parentNode.insertBefore(script, index)
-  
-// }
 
 export default MapPage;
