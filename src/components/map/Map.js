90587b59dd1008c25735f9f4b773789f063400c5
Kevin Sims <kevinsims1@outlook.com>
2019-09-05 12:28:50 -0500

    HERE api map uploaded

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import Location from '../location/location.js'
 import Nav from '../nav/Nav';
 import Sidebar from '../sidebar/Sidebar';
 import RoutingForm from './routingForm';
@@ -8,116 +9,116 @@ import "./Map.css"
 
 
 class MapPage extends Component {
-  constructor(){
-    super()
-    this.state = {
-      start: '',
-      end: '',
-      sidebarOpen: false,
-      directionsService: {},
-      directionsDisplay: {}
-    }
+    constructor(props) {
+      super(props);
+
+      this.platform = null;
+      this.map = null;
+
+      this.state = {
+          app_id: 'arvginqRfleoK8308SvJ',
+          app_code: 'sFz75OkwERmUxkBGeUjepg',
+          center: {
+              lat: 33.150673,
+              lng: -96.823608,
+          },
+          zoom: 7,
+          theme: props.theme,
+          style: props.style,
+      }
   }
 
-  componentDidMount() {
-    console.log("local storage token", localStorage);
-    this.setState({ sidebarOpen: !this.state.sidebarOpen }) 
-    this.renderMap()
-    this.walmart()
-  }
 
-  walmart = () => {
-    var coords = {
-      latitude: 41.839344, 
-      longitude: -87.65784,
-      distance: 6
-    }
-    return axios.post("http://eb-flask-rv-dev.us-east-1.elasticbeanstalk.com/fetch_walmart", coords)
-      .then(res => {
-        
-        let mart = {
-          lat: res.data[0].Latitude,
-          lng: res.data[0].Longitude
-        }
-  
-        this.initMap(mart)
-      })
-      .catch(err => {
-        console.log(err);
-      })
-
-  }
   toggleSidebar = ()=> {
     this.setState({ sidebarOpen: !this.state.sidebarOpen }) 
   }
 
-  renderMap = () => {
-    loadScript(`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLEMAP}&callback=initMap`)
-    window.initMap = this.initMap
-  }
+  
+  componentDidMount() {
+    this.platform = new window.H.service.Platform(this.state);
 
-  calculateAndDisplayRoute = (directionsS, directionsD) => {
-    directionsS.route({
-      origin: this.state.start,
-      destination: this.state.end,
-      travelMode: 'DRIVING'
-    }, function(response, status) {
-        if(status === 'OK') {
-          directionsD.setDirections(response)
-        } else {
-          window.alert('Directions request failed due to' + status)
-        }
+    var layer = this.platform.createDefaultLayers();
+    var container = document.getElementById('here-map');
 
-    })
-  }
-
-  initMap = (mart) => {
-    var directionsService = new window.google.maps.DirectionsService();
-    var directionsDisplay = new window.google.maps.DirectionsRenderer();
-    var map = new window.google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 8
-    });
-
-      this.setState({
-        directionsService,
-        directionsDisplay 
+    this.map = new window.H.Map(container, layer.normal.map, {
+        center: this.state.center,
+        zoom: this.state.zoom,
       })
 
+    var events = new window.H.mapevents.MapEvents(this.map);
+    // eslint-disable-next-line
+    var behavior = new window.H.mapevents.Behavior(events);
+    // eslint-disable-next-line
+    var ui = new window.H.ui.UI.createDefault(this.map, layer)
+}    
 
-    directionsDisplay.setMap(map)
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        var pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        };
-        //marker for users location
-        new window.google.maps.Marker({map:map, position: pos});
-        //new window.google.maps.Marker({map:map, position: mart});
-        map.setCenter(pos);
-      });
-    } else {
-      // Browser doesn't support Geolocation
-      console.log("Error finding location")
-    }
-    this.onChangeHandler();
-    
-    document.querySelector('form').addEventListener('submit', this.onChangeHandler)
-  }
-  routeChangeHandler = (e) => {
-   this.setState({
-     [e.target.name]: e.target.value
-    })
-  }
+  // renderMap = () => {
+  //   loadScript(`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLEMAP}&callback=initMap`)
+  //   window.initMap = this.initMap
+  // }
 
-  onChangeHandler = () => {
-    // e.preventDefault()
-    if(this.state.start.length > 0){
-      this.calculateAndDisplayRoute(this.state.directionsService, this.state.directionsDisplay)
-    }
+  // calculateAndDisplayRoute = (directionsS, directionsD) => {
+  //   directionsS.route({
+  //     origin: this.state.start,
+  //     destination: this.state.end,
+  //     travelMode: 'DRIVING'
+  //   }, function(response, status) {
+  //       if(status === 'OK') {
+  //         directionsD.setDirections(response)
+  //       } else {
+  //         window.alert('Directions request failed due to' + status)
+  //       }
+
+  //   })
+  // }
+
+  // initMap = (mart) => {
+  //   var directionsService = new window.google.maps.DirectionsService();
+  //   var directionsDisplay = new window.google.maps.DirectionsRenderer();
+  //   var map = new window.google.maps.Map(document.getElementById('map'), {
+  //     center: {lat: -34.397, lng: 150.644},
+  //     zoom: 8
+  //   });
+
+  //     this.setState({
+  //       directionsService,
+  //       directionsDisplay 
+  //     })
+
+
+  //   directionsDisplay.setMap(map)
+  //   if (navigator.geolocation) {
+  //     navigator.geolocation.getCurrentPosition(function(position) {
+  //       var pos = {
+  //         lat: position.coords.latitude,
+  //         lng: position.coords.longitude
+  //       };
+  //       //marker for users location
+  //       new window.google.maps.Marker({map:map, position: pos});
+  //       //new window.google.maps.Marker({map:map, position: mart});
+  //       map.setCenter(pos);
+  //     });
+  //   } else {
+  //     // Browser doesn't support Geolocation
+  //     console.log("Error finding location")
+  //   }
+  //   this.onChangeHandler();
     
-  }
+  //   document.querySelector('form').addEventListener('submit', this.onChangeHandler)
+  // }
+  // routeChangeHandler = (e) => {
+  //  this.setState({
+  //    [e.target.name]: e.target.value
+  //   })
+  // }
+
+  // onChangeHandler = () => {
+  //   // e.preventDefault()
+  //   if(this.state.start.length > 0){
+  //     this.calculateAndDisplayRoute(this.state.directionsService, this.state.directionsDisplay)
+  //   }
+    
+  // }
   render(){
   return (
     <div>
@@ -135,22 +136,22 @@ class MapPage extends Component {
       start={this.state.start}
       end={this.state.end}
       toggleSidebar = {this.toggleSidebar} sidebarOpen = {this.state.sidebarOpen} />
-      
-
-      <div id="map" ></div>
+      <Location />
+      <div id="here-map" style={{width: '100%', height: '400px', background: 'grey' }} />
+      {/* <div id="map" ></div> */}
     </div>
   );
 }
 }
 
-function loadScript(url){
-  var index = window.document.getElementsByTagName("script")[0]
-  var script = window.document.createElement("script")
-  script.src = url
-  script.async = true
-  script.defer = true
-  index.parentNode.insertBefore(script, index)
+// function loadScript(url){
+//   var index = window.document.getElementsByTagName("script")[0]
+//   var script = window.document.createElement("script")
+//   script.src = url
+//   script.async = true
+//   script.defer = true
+//   index.parentNode.insertBefore(script, index)
   
-}
+// }
 
 export default MapPage;
