7ce8b02ebc708867e0aa46ec4ebf1b1a0bdc67ca
Mitchell Robles <mitchellr92@live.com>
2019-09-10 09:27:21 -0700

    Commit message
    Co-authored-by: co-author <co-author-email>

--- src/components/auth/register/Register.js
+++ src/components/auth/register/Register.js
@@ -137,7 +137,7 @@ class RegisterForm extends Component {
 
   render() {
     const { errors } = this.state.credentials;
-    const isEnabled = this.state.credentials.username.length > 5 && this.state.credentials.email.length > 2 && this.state.credentials.password.length > 8;
+    const isEnabled = this.state.credentials.username.length >= 5 && this.state.credentials.email.length > 2 && this.state.credentials.password.length >= 8;
     return (
       <div>
         <Form>
